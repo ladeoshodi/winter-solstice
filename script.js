16169e3e9b6bced4868b02eaaf62b4c7b5b6ede9
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-12-06 19:08:48 +0000

    Switch landing page layout to grid

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const navLinks = document.querySelectorAll("#main-nav a");
 
+let currentSection;
+let nextSection;
+
+function hideCurrentSection() {
+  currentSection = Array.from(document.querySelectorAll("section")).filter(
+    (section) => {
+      return window.getComputedStyle(section).display !== "none";
+    }
+  )[0];
+  currentSection.style.display = "none";
+}
+
+function displayNextSection(e) {
+  e.preventDefault();
+  const section = e.target.hash;
+  nextSection = document.querySelector(section);
+  nextSection.style.display = "block";
+}
+
 navLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log(e.target.hash);
+    hideCurrentSection();
+    displayNextSection(e);
   });
 });
